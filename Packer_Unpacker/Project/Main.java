d8dc517463c4ecef2f80777eccd9d3e64414f839
yadavprerana455 <62585677+yadavprerana455@users.noreply.github.com>
2021-01-04 11:32:29 +0530

    Add files via upload

---
+++ Packer_Unpacker/Project/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.*;
+
+class Main()
+{
+	public static void main(String args[])
+	{
+		Scanner sobj=new Scanner(System.in);
+		System.out.println("..........................................................");
+			System.out.println("Marvellous Packer Unpacker");
+			System.out.println("..........................................................");
+			
+			
+			while(true)
+			{
+					System.out.println("..........................................................");
+					System.out.println("Enter your chioce\n");
+					System.out.println("1.Packing\n");
+					System.out.println("2.Unpacking\n");
+					System.out.println("3.Exit\n");
+					System.out.println("..........................................................");
+		
+			}
+		
+		
+	}
+}
